36a5f47f705516c0e35997996ce035bf5e5b769c
risandiro <151243329+risandiro@users.noreply.github.com>
2026-04-04 17:45:11 -0700

    Add CI workflow and smoke tests

--- auctions/tests.py
+++ auctions/tests.py
@@ -1,3 +1,25 @@
-from django.test import TestCase
+from django.test import Client, TestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class PublicPageTests(TestCase):
+    """Smoke tests: main routes respond without server errors."""
+
+    def setUp(self):
+        self.client = Client()
+
+    def test_index_returns_200(self):
+        response = self.client.get(reverse("index"))
+        self.assertEqual(response.status_code, 200)
+
+    def test_login_page_returns_200(self):
+        response = self.client.get(reverse("login"))
+        self.assertEqual(response.status_code, 200)
+
+    def test_register_page_returns_200(self):
+        response = self.client.get(reverse("register"))
+        self.assertEqual(response.status_code, 200)
+
+    def test_categories_returns_200(self):
+        response = self.client.get(reverse("categories"))
+        self.assertEqual(response.status_code, 200)
